294fddd5c77574b4eab5c30cc94966b0c0e12c7e
LukasOy <lukas.oylu@gmail.com>
2022-10-29 15:01:27 +0000

    commit numero x

--- src/app.js
+++ src/app.js
@@ -7,6 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let Num = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let relleno = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   let simbolo = ["corazon", "pica", "trebol", "diamante"];
+
+  let simbolorandom = simbolo[Math.floor(Math.random() * simbolo.length)];
+  let rellenorandome = relleno[Math.floor(Math.random() * relleno.length)];
+
+  console.log(rellenorandome, simbolorandom);
+
+  let carta = document.querySelector(".carta");
+  carta[1].innerHTML = relleno[rellenorandome];
+  carta[1].innerHTML = simbolo[simbolorandom];
 };
