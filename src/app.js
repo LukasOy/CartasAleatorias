1721a3f907d5ec28a3fd97bcbf3213fb3903f290
LukasOy <lukas.oylu@gmail.com>
2022-10-29 13:18:06 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,6 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let Num = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let simbolo = ["corazon", "pica", "trebol", "diamante"];
 };
