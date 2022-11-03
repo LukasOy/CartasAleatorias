00b4933bf57d42e1694294dae1a941c3b21b257a
LukasOy <lukas.oylu@gmail.com>
2022-11-03 20:13:50 +0000

    crudo pero listo

--- src/app.js
+++ src/app.js
@@ -8,14 +8,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   let relleno = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-  let simbolo = ["corazon", "pica", "trebol", "diamante"];
+  let simbolo = ["♥", "♠", "♣", "♦"];
 
   let simbolorandom = simbolo[Math.floor(Math.random() * simbolo.length)];
-  let rellenorandome = relleno[Math.floor(Math.random() * relleno.length)];
+  let rellenorandom = relleno[Math.floor(Math.random() * relleno.length)];
 
-  console.log(rellenorandome, simbolorandom);
+  console.log(rellenorandom, simbolorandom);
+  let Simbolo1 = document.querySelector("#carta1");
+  Simbolo1.innerHTML = simbolorandom;
+  let SimboloMedio = document.querySelector("#SimboloMedio");
+  SimboloMedio.innerHTML = rellenorandom;
+  let Simbolo11 = document.querySelector("#carta2");
+  Simbolo11.innerHTML = simbolorandom;
 
-  let carta = document.querySelector(".carta");
-  carta[1].innerHTML = relleno[rellenorandome];
-  carta[1].innerHTML = simbolo[simbolorandom];
+  if (simbolorandom == "♥" || simbolorandom == "♦") {
+    document.querySelector("#carta1").style.color = "red";
+    document.querySelector("#carta2").style.color = "red";
+  }
 };
+/*
+let Simbolo11 = document.querySelector("#carta2");
+  Simbolo11.innerHTML = simbolorandom;
+
+  let cambioarojo = document.querySelector("#carta1");
+    cambioarojo.classList.add("red");
+
+}; 
+  */
